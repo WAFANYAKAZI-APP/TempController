e61e8efab23b6f372ac0cb55ab25aac0775d02e8
b-lema <lema@adanianlabs.io>
2022-09-24 14:48:46 +0300

    TempController

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [temp ,setTemp] = useState(10);
+    const [tempColour, setTempColor] = useState("cold")
+
+    const inTemperature = () =>{
+        if(temp === 30) return;
+
+        if (temp >= 15){
+            setTempColor("hot")
+        }
+        setTemp(temp + 1)
+    }
+    const deTemperature = () =>{
+        if(temp === 0) return;
+        if (temp <= 15){
+            setTempColor("cold")
+        }
+        setTemp(temp - 1)
+    }
+    return (
+
+        <div className="app-container">
+            <div className="temp-display-container">
+                <div className={` temp-display ${tempColour}`}>
+                    {temp}°
+                </div>
+            </div>
+            <div className="button-container">
+                <button onClick={()=> inTemperature()}>+</button>
+                <button onClick={()=> deTemperature()}>-</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
